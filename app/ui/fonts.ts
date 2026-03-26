eae3c6fd7e97457a535257fc99dd6a1d3193bf84
xuyang <843760641@qq.com>
2026-03-26 16:41:17 +0800

    init

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const font = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+});
